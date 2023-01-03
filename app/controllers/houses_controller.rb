9bb55cc7c0b0dbc52a3140e2944486989025497c
mutsumisadara <mutsumisadara@gmail.com>
2023-01-03 20:38:01 +0900

    change form to ja

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -1,25 +1,22 @@
 class HousesController < ApplicationController
   before_action :set_house, only: %i[ show edit update destroy ]
-
-  # GET /houses or /houses.json
   def index
     @houses = House.all
   end
 
-  # GET /houses/1 or /houses/1.json
   def show
+    @house = House.find(params[:id])
   end
 
-  # GET /houses/new
   def new
     @house = House.new
   end
 
-  # GET /houses/1/edit
   def edit
+    @house = House.find(params[:id])
+    # 2.times { @house.stations.build }
   end
 
-  # POST /houses or /houses.json
   def create
     @house = House.new(house_params)
 
@@ -34,7 +31,6 @@ class HousesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /houses/1 or /houses/1.json
   def update
     respond_to do |format|
       if @house.update(house_params)
@@ -47,9 +43,9 @@ class HousesController < ApplicationController
     end
   end
 
-  # DELETE /houses/1 or /houses/1.json
   def destroy
     @house.destroy
+    redirect_to houses_path, notice: "削除しました"
     respond_to do |format|
       format.html { redirect_to houses_url, notice: "House was successfully destroyed." }
       format.json { head :no_content }
@@ -57,12 +53,10 @@ class HousesController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_house
       @house = House.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def house_params
       params.require(:house).permit(:name, :price, :address, :age, :information)
     end
